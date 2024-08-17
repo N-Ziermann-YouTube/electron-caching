e10b03b1ef553aaccbccbf02b104c1786b496d86
Niklas Ziermann <n-ziermann@protonmail.com>
2024-08-17 12:00:52 +0200

    Initial commit

---
+++ src/electron/main.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron';
+import { ipcMainHandle, ipcWebContentsSend, isDev } from './util.js';
+import { getPreloadPath, getUIPath } from './pathResolver.js';
+import { DEV_PORT } from './constants.js';
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
+
+  mainWindow.loadURL(`http://localhost:${DEV_PORT}`);
+
+  ipcMainHandle('requestExample', () => {
+    return { message: 'Hello World' };
+  });
+
+  let changingMessageNumber = 1;
+  setInterval(() => {
+    ipcWebContentsSend('exampleChanged', mainWindow.webContents, {
+      message: `Message Number: ${changingMessageNumber}`,
+    });
+    changingMessageNumber++;
+  }, 500);
+});
